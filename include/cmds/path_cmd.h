fed98b8d3b7144740de781a6e184a453c66fc970
Jorge Mederos <jmederosalvarado@estudiantes.matcom.uh.cu>
2019-09-22 17:29:24 -0400

    Add path_cmd declaration

---
+++ include/cmds/path_cmd.h
@@ -0,0 +1,19 @@
+#ifndef PATH_CMD_H
+#define PATH_CMD_H
+
+#include <cmd.h>
+
+struct vector;
+
+struct path_cmd
+{
+    struct cmd base;
+    struct vector *args;
+};
+
+void path_cmd_init(struct path_cmd *c);
+void path_cmd_free(struct path_cmd *c);
+
+int path_cmd_run(struct command *c);
+
+#endif
