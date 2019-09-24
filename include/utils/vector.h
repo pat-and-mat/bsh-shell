cce70bc36fec747bb3c42beb44b445b8b86bc1c3
Jorge Mederos <jmederosalvarado@estudiantes.matcom.uh.cu>
2019-09-24 09:28:25 -0400

    Fix some malloc size in vector

--- include/utils/vector.h
+++ include/utils/vector.h
@@ -9,11 +9,11 @@ struct vector
     size_t item_size;
 };
 
-void vector_init(struct vector *, size_t);
+void vector_init(struct vector *);
 int vector_count(struct vector *);
 static void vector_resize(struct vector *, int);
-void vector_add(struct vector *, void *);
-void vector_set(struct vector *, int, void *);
+void vector_add(struct vector *, void *, size_t);
+void vector_set(struct vector *, int, void *, size_t);
 void *vector_get(struct vector *, int);
 void vector_delete(struct vector *, int);
 void vector_free(struct vector *);
--- src/utils/vector.c
+++ src/utils/vector.c
@@ -6,12 +6,11 @@
 
 #define VECTOR_INIT_CAPACITY 5
 
-void vector_init(struct vector *v, size_t item_size)
+void vector_init(struct vector *v)
 {
     v->capacity = VECTOR_INIT_CAPACITY;
     v->count = 0;
     v->items = malloc(sizeof(void *) * v->capacity);
-    v->item_size = item_size;
 }
 
 int vector_count(struct vector *v)
@@ -29,24 +28,24 @@ static void vector_resize(struct vector *v, int capacity)
     }
 }
 
-void vector_add(struct vector *v, void *item)
+void vector_add(struct vector *v, void *item, size_t item_size)
 {
     if (v->capacity == v->count)
         vector_resize(v, v->capacity * 2);
-    void *item_cpy = malloc(v->item_size);
-    memcpy(item_cpy, item, v->item_size);
+    void *item_cpy = malloc(item_size);
+    memcpy(item_cpy, item, item_size);
     v->items[v->count++] = item_cpy;
 }
 
-void vector_set(struct vector *v, int index, void *item)
+void vector_set(struct vector *v, int index, void *item, size_t item_size)
 {
     if (index >= 0 && index < v->count)
     {
         free(v->items[index]);
         v->items[index] = item;
     }
-    void *item_cpy = malloc(v->item_size);
-    memcpy(item_cpy, item, v->item_size);
+    void *item_cpy = malloc(item_size);
+    memcpy(item_cpy, item, item_size);
     v->items[index] = item_cpy;
 }
 
