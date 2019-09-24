cb4ea0f93d4b0cf45f01de10aebfb1a981d1ea9f
Jorge Mederos <jmederosalvarado@estudiantes.matcom.uh.cu>
2019-09-24 19:10:43 -0400

    Add xmemory implementation file

---
+++ src/utils/xmemory.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+#include <utils/xmemory.h>
+
+#define OFFSET sizeof(size_t)
+
+void *xmalloc(size_t size)
+{
+    size_t *ptr = malloc(OFFSET + size);
+    *ptr = size;
+    return ptr + 1;
+}
+
+void *xrealloc(void *ptr, size_t size)
+{
+    if (ptr == NULL)
+        return ptr = xmalloc(size);
+    size_t *sptr = (size_t *)ptr;
+    sptr--;
+    *sptr = size;
+    sptr = realloc(sptr, OFFSET + size);
+    if (sptr == NULL)
+        return NULL;
+    return sptr + 1;
+}
+
+void xfree(void *ptr)
+{
+    free((size_t *)ptr - 1);
+}
+
+size_t xsize_of(void *ptr)
+{
+    return *((size_t *)ptr - 1);
+}
