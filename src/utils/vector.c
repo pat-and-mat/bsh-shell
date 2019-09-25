8669b8a686162455d90ec6466673c38778cf3fc3
Jorge Mederos <jmederosalvarado@estudiantes.matcom.uh.cu>
2019-09-25 18:09:39 -0400

    Fix non returning vector_copy method

--- src/utils/vector.c
+++ src/utils/vector.c
@@ -19,12 +19,13 @@ struct vector *vector_copy(struct vector *v)
 {
     struct vector *cpy = malloc(sizeof(struct vector));
     cpy->capacity = v->capacity;
-    cpy->count = v->count;
+    cpy->count = 0;
     cpy->free_item = v->free_item;
     cpy->copy_item = v->copy_item;
     cpy->items = malloc(sizeof(void *) * cpy->capacity);
     for (int i = 0; i < v->count; i++)
         vector_add(cpy, v->items[i]);
+    return cpy;
 }
 
 int vector_count(struct vector *v)
@@ -47,7 +48,7 @@ void vector_add(struct vector *v, void *item)
     if (v->capacity == v->count)
         vector_resize(v, v->capacity * 2);
 
-    v->items[v->count++] = v->copy_item(item);
+    v->items[v->count++] = (*v->copy_item)(item);
 }
 
 void vector_set(struct vector *v, int index, void *item)
@@ -57,7 +58,7 @@ void vector_set(struct vector *v, int index, void *item)
         free(v->items[index]);
         v->items[index] = item;
     }
-    v->items[index] = v->copy_item(item);
+    v->items[index] = (*v->copy_item)(item);
 }
 
 void *vector_get(struct vector *v, int index)
@@ -73,7 +74,7 @@ void vector_delete(struct vector *v, int index)
         return;
 
     if (v->free_item != NULL)
-        v->free_item(v->items[index]);
+        (*v->free_item)(v->items[index]);
     free(v->items[index]);
     v->items[index] = NULL;
 
@@ -94,7 +95,7 @@ void vector_free(struct vector *v)
     for (int i = 0; i < v->count; i++)
     {
         if (v->free_item != NULL)
-            v->free_item(v->items[i]);
+            (*v->free_item)(v->items[i]);
         free(v->items[i]);
         v->items[i] = NULL;
     }
