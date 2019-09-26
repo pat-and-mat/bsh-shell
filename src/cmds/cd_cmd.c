12bd7407918a66270235b142b28ae58440a16d05
Rodrigo <rodrigo.garcia@estudiantes.matcom.uh.cu>
2019-09-26 18:31:33 -0400

    Add cd_cmd

---
+++ src/cmds/cd_cmd.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <cmds/cmd.h>
+#include <cmds/cd_cmd.h>
+#include <utils/vector.h>
+#include <utils/xmemory.h>
+
+struct cd_cmd *cd_cmd_init()
+{
+    struct cd_cmd *cd_cmd = xmalloc(sizeof(struct cd_cmd));
+    cd_cmd_init_allocated(cd_cmd);
+    return cd_cmd;
+}
+
+void cd_cmd_init_allocated(struct cd_cmd *c)
+{
+    cmd_init_allocated(&c->base, CMD_T_CD, cd_cmd_run);
+}
+
+struct cmd *cd_cmd_set_arg(struct cd_cmd *c, char *arg)
+{
+    strcpy(c->arg, arg);
+    return c->arg;
+}
+
+struct cmd *cd_cmd_get_arg(struct cd_cmd *c)
+{
+    return c->arg;
+}
+
+int cd_cmd_run(struct cmd *c)
+{
+    struct cd_cmd *cd_cmd = (struct cd_cmd *)c;
+    return 0;
+}
