4d447b0100c876e29ecc8dc4b1a591793cb90171
Jorge Mederos <jmederosalvarado@estudiantes.matcom.uh.cu>
2019-09-29 22:33:33 -0400

    Implement cd command

--- src/cmds/cd_cmd.c
+++ src/cmds/cd_cmd.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 #include <cmds/cmd.h>
 #include <cmds/cd_cmd.h>
@@ -22,7 +25,24 @@ void cd_cmd_init_allocated(struct cd_cmd *c)
 
 bool cd_cmd_run(struct cmd *c)
 {
-    return true;
+    struct cd_cmd *cd = (struct cd_cmd *)c;
+
+    int saved_stdout = dup(STDOUT_FILENO);
+
+    for (int i = 0; i < vector_count(cd->base.redirects); i++)
+        cmd_run(vector_get(cd->base.redirects, i));
+
+    if ((vector_count(cd->base.args) == 1 && chdir(getpwuid(getuid())->pw_dir) == 0) ||
+        (vector_count(cd->base.args) == 2 && chdir(vector_get(cd->base.args, 1)) == 0))
+    {
+        dup2(saved_stdout, STDOUT_FILENO);
+        close(saved_stdout);
+        return true;
+    }
+
+    dup2(saved_stdout, STDOUT_FILENO);
+    close(saved_stdout);
+    return false;
 }
 
 void cd_cmd_print(struct cmd *c)
--- src/main.c
+++ src/main.c
@@ -43,6 +43,9 @@ int main(int argc, char *argv[])
         {
             cmd_print(cmd);
             printf("\n");
+
+            if (!cmd_run(cmd))
+                printf("Command execution failed\n");
         }
 
         xmem_free();
