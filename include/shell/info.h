72647cd7475c8834f3cf25950c37c179a5468322
Jorge Mederos <jmederosalvarado@estudiantes.matcom.uh.cu>
2019-10-03 17:19:17 -0400

    Implement info

--- include/shell/info.h
+++ include/shell/info.h
@@ -2,9 +2,12 @@
 #define INFO_H
 
 #include <termios.h>
+#include <stdbool.h>
 
 int shell_terminal;
 struct termios shell_tmodes;
 struct termios default_tmodes;
+
+bool shell_info_init();
 
 #endif
